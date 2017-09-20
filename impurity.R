58b203a9dc0c81b3694244b541fde49858c73b8a
Marcel Visser <marcel1337@hotmail.com>
2017-09-20 15:32:40 +0200

    Basic documentation

--- impurity.R
+++ impurity.R
@@ -1,10 +1,20 @@
 library(data.tree)
-impurity <- function(x){
-  partition0 <- length(x[x == 0])/length(x)
-  partition1 <- length(x[x == 1])/length(x)
-  return(partition0 * partition1)
+
+##
+# We use the gini-index as described. This is the R-translation within our structure.
+# Given a node x, we determine the impurity with formula i(x) = p(0|x)p(1|x) (two-class Gini-index).
+# This function does that by counting the 0 or 1 classes and calculating the frequency and then using the formula.
+##
+tree.impurity <- function(x){
+  zero_frequency <- length(x[x == 0])/length(x)
+  one_frequency <- length(x[x == 1])/length(x)
+  return(zero_frequency * one_frequency)
 }
 
+
+##
+# Grows the tree
+##
 tree.grow <- function(x,y,nmin,minleaf,nfeat){
   nodelist <- x
   tree <- Node$new("test1")
@@ -19,7 +29,10 @@ tree.grow <- function(x,y,nmin,minleaf,nfeat){
   }
   print(tree)
 }
-#Rloops should be avoided
+##
+# Calculates best split
+# todo: describe
+##
 calculate.best.split <- function(x,y){
   currentImp = impurity(t(y))
   splitlist <- vector()
@@ -30,19 +43,31 @@ calculate.best.split <- function(x,y){
   print(max(t(max(splitlist))))
 }
 
+##
+# Calculates redistribution error with the formula given in the lecture notes
+# todo: define parameters and explain code
+##
 redistribution <- function(x,y){
   currentImp <- impurity(y)
   redis <- currentImp - (length(x[x==0])/length(x))*impurity(y[which(x==0)]) - 
     (length(x[x==1])/length(x))*impurity(y[which(x==1)])
   return(redis)
 }
-#tree.grow(credit.dat[1:5],credit.dat[6],0,0,0)
+
+##
+# Creates a comparison matrix for finding the best split.
+# todo: elaborate
+##
 createCompMatrix <- function(x,y){
   compmatrixx1 <- expand.grid(x,getsplits(x))
   splitmatrix <- matrix(as.numeric(compmatrixx1[,1] <= compmatrixx1[,2]), nrow=length(x))
   impurity <- apply(splitmatrix,2, y=y, function(splitmatrix,y) redistribution(splitmatrix,y))
   return(c(getsplits(x)[which.max(impurity)],max(impurity)))
 }
+##
+# Splits a node depending on the classes. If the node is pure, then return the node. Otherwise split.
+# todo: elaborate
+##
 getsplits <- function(x){
   if(length(unique(x))>1){
     m <- embed(unique(sort(x)),2)
@@ -53,6 +78,10 @@ getsplits <- function(x){
   }
 }
 
+##
+# Meat of the program.
+# todo: elaborate
+##
 build.tree <- function(x,y,result){
   if(impurity(y)>0){
     result1 <- (apply(x,2,y=y,function(x,y) createCompMatrix(x,t(y))))
@@ -73,7 +102,11 @@ build.tree <- function(x,y,result){
     return(result)
   }
 }
+
+##Debug code
 print(build.tree(credit.dat[1:5],credit.dat[6], c()))
+
+#Snippets
 
 # <-trainingset[-(which.max(result1[2,])),which(trainingset[which.max(result1[2,])]<=result1[1,which.max(result1[2,])])]
 #rowschild1 = which(trainingset[which.max(result1[2,])]<=result1[1,which.max(result1[2,])])
