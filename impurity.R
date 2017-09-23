82e38a08f16f5ee265b21b5152132d1fd495fd43
DESKTOP-6DB3AGA\Yannick Mijsters <y.mijsters@gmail.com>
2017-09-23 09:33:46 +0200

    All parmeters kind of working. To do: fix indexing issues with minleaf and nfeat and more comments

--- impurity.R
+++ impurity.R
@@ -14,45 +14,17 @@ tree.impurity <- function(x){
   return(zero_frequency * one_frequency)
 }
 
-
-##
-# Grows the tree
-##
-tree.grow <- function(x,y,nmin,minleaf,nfeat){
-  nodelist <- x
-  tree <- Node$new("test1")
-  teller<-0
-  for(i in x){
-    if(impurity(t(y))>0){
-      bestsplit<-calculate.best.split(i,y)
-      tree<-tree$AddChild(teller)
-      print(tree)
-      teller<-teller+1
-    }
-  }
-  print(tree)
-}
-##
-# Calculates best split
-# todo: describe
-##
-calculate.best.split <- function(x,y){
-  currentImp = impurity(t(y))
-  splitlist <- vector()
-  split <- t(unique(x))
-  if(length(x[x<=split]) > 0&& length(x[x>split])>0){
-    splitlist <- c(splitlist, redistribution(x,y,split,currentImp))
-  }
-  print(max(t(max(splitlist))))
-}
-
 ##
 # Calculates redistribution error with the formula given in the lecture notes
-# todo: define parameters and explain code
+# x: all the trainingsamples for one variable and 
+# y: is the classification of these samples
+# Redistribution error is calculated using Rerror = i(x) - P(x | y = 0)*i(x | y=0) - P(x | y = 1)*i(x | y = 1) 
+# With P being the partition and i the impurity
+# return: the distribution error
 ##
 redistribution <- function(x,y){
-  currentImp <- tree.impurity(y)
-  redis <- currentImp - (length(x[x==0])/length(x))*tree.impurity(y[which(x==0)]) - 
+  currentImpurity <- tree.impurity(y)
+  redis <- currentImpurity - (length(x[x==0])/length(x))*tree.impurity(y[which(x==0)]) - 
     (length(x[x==1])/length(x))*tree.impurity(y[which(x==1)])
   return(redis)
 }
@@ -61,9 +33,14 @@ redistribution <- function(x,y){
 # Creates a comparison matrix for finding the best split.
 # todo: elaborate
 ##
-createCompMatrix <- function(x,y){
+createCompMatrix <- function(x,y, minleaf){
   compmatrixx1 <- expand.grid(x,getsplits(x))
   splitmatrix <- matrix(as.numeric(compmatrixx1[,1] <= compmatrixx1[,2]), nrow=length(x))
+  allowedSplitsWithMinLeaf = (which(colSums(splitmatrix) >= minleaf & colSums(splitmatrix) <= length(x) - minleaf))
+  if(length(allowedSplitsWithMinLeaf) == 0 ){
+    return(c(0,0))
+  }
+  splitmatrix <- matrix(splitmatrix[, allowedSplitsWithMinLeaf], ncol = length(allowedSplitsWithMinLeaf))
   impurity <- apply(splitmatrix,2, y=y, function(splitmatrix,y) redistribution(splitmatrix,y))
   return(c(getsplits(x)[which.max(impurity)],max(impurity)))
 }
@@ -73,28 +50,37 @@ createCompMatrix <- function(x,y){
 ##
 getsplits <- function(node){
   if(length(unique(node))>1){
-    m <- embed(unique(sort(node)),2)
-    return(rowMeans(m))
+    splitCalculationMatrix <- embed(unique(sort(node)),2)
+    return(rowMeans(splitCalculationMatrix))
   }
   else{
     return(unique(node))
   }
 }
 
-getnewnode <- function(data,class){
-  result <- (apply(data,2,y=class,function(x,y) createCompMatrix(x,t(y))))
+getnewnode <- function(data,class, minleaf, nfeat){
+  print(c(1:ncol(data)))
+  selectedColumns <- sample(c(1:ncol(data)), min(nfeat,ncol(data)))
+  datawithnfeat <- data[, selectedColumns]
+  print(datawithnfeat)
+  result <- (apply(data,2,y=class, minleaf = minleaf, function(x,y, minleaf) createCompMatrix(x,t(y), minleaf)))
   bestsplitcolumn <- which.max(result[2,])
   bestsplit <-result[1,bestsplitcolumn]
-  return(list(bestsplit, bestsplitcolumn, c(which(data[bestsplitcolumn] <= bestsplit)), c(which(data[bestsplitcolumn] > bestsplit))))
+  if(bestsplit == 0){
+    return(list(0))
+  }
+  else{
+    return(list(bestsplit, bestsplitcolumn, c(which(data[bestsplitcolumn] <= bestsplit)), c(which(data[bestsplitcolumn] > bestsplit))))
+  }
 }
 
 
 ##
-# Meat of the program.
+# Meat of the program recursive tree builder.
 # todo: implement parameters
 # The parameters are:
-# data: the columns containing the dataset without the class that needs to be split on
-# class: the column containing only the class that needs to be split on
+# data: the columns containing the dataset without the class that needs to be split on --> Should be called x
+# class: the column containing only the class that needs to be split on --> Should be called y
 # nmin: the minimum amount of observations in a node required to split
 # minleaf: the minimum amount of observations allowed in a node
 # nfeat: the amount of features that should be looked at for each split
@@ -102,21 +88,28 @@ getnewnode <- function(data,class){
 build.tree <- function(data, class, nmin, minleaf, nfeat){
   #if there is impurity and there are enough observations for a split, then do so
   if(tree.impurity(class) > 0 && nrow(data) > nmin){
-    result1 <- getnewnode(data,class)
-    result <- Node$new(colnames(data)[result1[[2]]], split = result1[[1]], samples = rownames(data), featurecolumn = result1[[2]])
-    newclass1 <- t(class)[result1[[3]]]
-    newdata1 <- data[result1[[3]],-result1[[2]]]
-    newclass2 <- t(class)[result1[[4]]]
-    newdata2 <- (data[result1[[4]],-result1[[1]]])
-    #Add child nodes from the split and recursively continue
-    result$AddChildNode(build.tree(newdata1, newclass1, nmin, minleaf, nfeat))
-    result$AddChildNode(build.tree(newdata2, newclass2, nmin, minleaf, nfeat))
-    return(result)
+    result1 <- getnewnode(data,class, minleaf, nfeat)
+    if(result1[[1]] != 0){
+      result <- Node$new(colnames(data)[result1[[2]]], split = result1[[1]], samples = rownames(data), featurecolumn = result1[[2]])
+      newclass1 <- t(class)[result1[[3]]]
+      newdata1 <- data[result1[[3]],-result1[[2]]]
+      newclass2 <- t(class)[result1[[4]]]
+      newdata2 <- (data[result1[[4]],-result1[[1]]])
+      #Add child nodes from the split and recursively continue
+      result$AddChildNode(build.tree(newdata1, newclass1, nmin, minleaf, nfeat))
+      result$AddChildNode(build.tree(newdata2, newclass2, nmin, minleaf, nfeat))
+      return(result)
+    }
   }
-  else{
-    return(Node$new(paste("Leaf", paste(rownames(data), collapse=",")), split= "Leaf", samples= rownames(data)))
-  }
+  return(Node$new(paste("Leaf", paste(rownames(data), collapse=",")), split= "Leaf", samples= rownames(data)))
+}
+##
+# Classify a trainingssample on a decision tree
+##
+tree.classify <- function(x, tree){
+  
 }
 
+
 ##Debug code
-print(build.tree(credit.dat[1:5],credit.dat[6], 1, 1, 1), "split", "samples", "featurecolumn")
+print(build.tree(credit.dat[1:5],credit.dat[6], 1, 0, 5), "split", "samples", "featurecolumn")
